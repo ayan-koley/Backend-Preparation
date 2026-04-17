8386451c455d0035f0ba1f72f7370693391667c0
Ayan Koley <koleyayan07@gmail.com>
2026-04-17 13:07:13 +0530

    Add comparison examples in 04_comparision.js

---
+++ JAVASCRIPT/JS-Fundamentals/04_comparision.js
@@ -0,0 +1,18 @@
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 >= 2);
+// console.log(2 <= 1);
+// console.log(2 == 2);
+
+// console.log("2" > 1);
+// console.log("02" < 1);
+
+// null checking
+
+// console.log(null > 0);
+// console.log(null < 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// === (datatype + value) strict check 
+console.log("2" === 2);
