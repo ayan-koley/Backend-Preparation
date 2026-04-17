ba3102d898543c645e4112c492386ac7231d4cdf
Ayan Koley <koleyayan07@gmail.com>
2026-04-17 11:29:57 +0530

    Add initial variable declarations and logging in 01_variable.js

---
+++ JAVASCRIPT/JS-Fundamentals/01_variable.js
@@ -0,0 +1,13 @@
+const accountId = 122343;
+let accountEmail = "ayan@gogle.com"
+var accountPassword = "India"
+accountCity = "Mahishadal"
+
+// accountId = 5434; // not allowed 
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
+
+/*
+    var is not recommended to use because of its function scope and hoisting behavior.
+*/
