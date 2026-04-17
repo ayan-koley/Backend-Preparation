a4880e776e2de6c0e2830039053cabd3471bd097
Ayan Koley <koleyayan07@gmail.com>
2026-04-17 13:03:00 +0530

    Add conversion operations examples in 03_conversionOperations.js

---
+++ JAVASCRIPT/JS-Fundamentals/03_conversionOperations.js
@@ -0,0 +1,42 @@
+let temperature = "33";
+
+// console.log(typeof temperature); // output: string
+
+// Convert the string to a number
+let tempInNumber = Number(temperature);
+// console.log(typeof tempInNumber); // output: number
+// console.log(tempInNumber); // output: 33
+
+/*
+    "33" => number => 33
+    "33abc" => number => NaN
+    true => number => 1
+    undefined => number => NaN
+    null => number => 0
+*/
+
+// ***************** Operations *****************
+
+let value = 3;
+let newValue = -value;
+// console.log(newValue);
+
+// console.log(2*2);
+// console.log(2-2);
+// console.log(2**3);
+// console.log(2+2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Hello";
+let str2 = " ayan";
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 3);
+// console.log(1 + 2 + "3");
+
+// console.log(+true);
+// console.log(+false);
