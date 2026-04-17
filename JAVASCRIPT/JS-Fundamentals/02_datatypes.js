230ee8529e34a58945178fe792f6aa84b2989a5f
Ayan Koley <koleyayan07@gmail.com>
2026-04-17 12:29:00 +0530

    Add data types examples and logging in 02_datatypes.js

---
+++ JAVASCRIPT/JS-Fundamentals/02_datatypes.js
@@ -0,0 +1,14 @@
+"use strict"; // treat all JS code as newer version
+
+let fullName = "Ayan Koley"; // string
+let age = 24; // number
+let isProgrammer = true; // boolean
+let language; // undefined
+let nullValue = null; // null
+
+let array = [1, 2, 3, 4, 5]; // array
+let object = { name: "Ayan", age: 24 }; // object
+
+// store of undefined and null
+console.log(typeof undefined); // undefined 
+console.log(typeof null); // object
