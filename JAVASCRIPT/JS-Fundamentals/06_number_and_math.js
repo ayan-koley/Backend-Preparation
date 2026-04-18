f467be483271642aa2fb23f1b518c2c181aae649
Ayan Koley <koleyayan07@gmail.com>
2026-04-18 19:49:12 +0530

    Add number and math examples in 06_number_and_math.js

---
+++ JAVASCRIPT/JS-Fundamentals/06_number_and_math.js
@@ -0,0 +1,22 @@
+const score = 400;
+console.log(score);
+
+const balance = new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toFixed(2));
+// console.log(balance.toLocaleString());
+
+const otherNumber = 123.89485;
+// console.log(otherNumber.toPrecision(3));
+
+
+// +++++++++++++++++++++++++ Math +++++++++++++++++++++++++
+
+console.log(Math.floor(1.3));
+console.log(Math.round(1.2));
+console.log(Math.ceil(1.2));
+console.log(Math.random());
+console.log(Math.floor(4.7));
+console.log(Math.floor(Math.random() * (30 - 20 + 1)) + 20);
