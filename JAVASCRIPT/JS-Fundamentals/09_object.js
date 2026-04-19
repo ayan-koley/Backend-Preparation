72cbb20aa3b5f395d00228e631d305d9a12a5b0b
Ayan Koley <koleyayan07@gmail.com>
2026-04-19 10:28:59 +0530

    Add object manipulation examples in 09_object.js

---
+++ JAVASCRIPT/JS-Fundamentals/09_object.js
@@ -0,0 +1,37 @@
+// singleton
+// Object.create()
+
+// Object literals
+const mySym = Symbol("key1");
+const JsUser = {
+    name: "ayan koley",
+    email: "ayan@google.com",
+    "full name": "Ayan Koley",
+    [mySym]: "Mykey1"
+}
+// console.log(JsUser.name);
+// console.log(JsUser["name"]); // square notation
+// console.log(JsUser["full name"]); // can't get the data by dot operator
+// JsUser.age = 16;
+// Object.freeze(JsUser);
+// JsUser.age = 34;
+// console.log(JsUser)
+
+JsUser.greeting = function() {
+    console.log(`Hello from Js user ${this["full name"]}`);
+}
+
+// JsUser.greeting();
+
+
+const singleTonObj = new Object();
+singleTonObj.name = "Singleton Object";
+
+const obj1 = { 1: 'ayan', 2: 'arpita' };
+const obj2 = { 3: 'sayan', 4: 'anik'};
+
+const obj3 = Object.assign({}, obj1, obj2);
+// console.log(obj3);
+
+const spreadObj = { ...obj1, ...obj2 };
+// console.log(spreadObj);
